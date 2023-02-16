3960418ebca16037f89810f5eccf3dac9cbe9136
Yash Bhayani <yashbhayani3007@gmail.com>
2023-02-16 13:07:24 +0530

    first commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public string test()
+        {
+            return "okay evrything is working fine.";
+        }
+
+        public int data(int l)
+        {
+            return l;
+        }
+    }
+}
---
+++ Librarymethode/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Librarymethode.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Class1 oop = new Class1();
+
+            ViewBag.Message = "Your application description page. it is working fine okay." ;
+            ViewBag.Yash = oop.test();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            Class1 oop = new Class1();
+
+            int k = oop.data(12);
+            ViewBag.UU = k;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
